90b88d68ff5e9c586ab6aca16f29b30f674694a2
Vibhash Mishra <vibhashmishra710@gmail.com>
2026-03-15 04:05:24 +0530

    chore: initial project structure and auth setup

--- app/page.js
+++
@@ -1,8 +0,0 @@
-import { prisma } from "@/lib/prisma"
-
-export default async function Home() {
-  const users = await prisma.user.findMany()
-  console.log(users)
-
-  return <div>GateDrop</div>
-}
