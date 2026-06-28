68f4cf700023a512a9be6734ba49d11b975e0aee
Vibhash Mishra <vibhashmishra710@gmail.com>
2026-06-28 21:19:21 +0530

    fix: redirect authenticated users to landing page from auth pages

--- proxy.js
+++ proxy.js
@@ -27,7 +27,7 @@ export default auth((req) => {
   }
 
   if (isLoggedIn && hasRole && isOnOnboarding) {
-    return NextResponse.redirect(new URL("/dashboard", req.url))
+    return NextResponse.redirect(new URL("/", req.url))
   }
 
   return NextResponse.next()
